17ea37a3eb7f4bedb94f8f8930cc29466c357d4f
Ben Kelcher <benkelcher77@gmail.com>
2022-08-16 14:00:07 -0400

    Simple market data viewer using yfinance.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import yfinance as yf
 
 app = Flask(__name__)
 
-@app.route('/')
-def helloWorld():
-    return "Hello, World!"
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=["POST", "GET"])
+def result():
+    if request.method == "POST":
+        result = request.form.to_dict()
+        t = yf.Ticker(result["ticker"])
+        return render_template("result.html", result=t.info, ticker=result["ticker"])
 
 if __name__ == "__main__":
     app.run()
